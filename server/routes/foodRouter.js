376a3ac467cc81b3b3abfad3f71fc7e9c2823540
Mardin <mardineliassi57@gmail.com>
2023-08-17 22:41:52 +0200

    deleted file, and more work on the productRouter

--- server/routes/foodRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.post("/food", (req,res) => {
-    res.send("");
-});
-
-export default router;
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -11,6 +11,12 @@ router.get("/products", async (req,res) => {
       
 });
 
+router.get("/products/:id", async (req,res) => {
+    const [product] = await db.query(
+        "SELECT * from products where id = ?",[req.params.id]);
+        res.send({product: product});
+});
+
 router.post("/products", async (req,res) => {
     const { name, price, weight, supplier,discount } = req.body;
   try {
@@ -22,6 +28,34 @@ router.post("/products", async (req,res) => {
 } catch(error){
     res.status(400).send("Something went wrong.")
 }});
+
+router.put('/products/:id', async (req, res) => {
+    try {
+      const {name,price,weight,supplier,discount} = req.body;
+      
+      const productId = req.params.id;
+      const updateProduct = await db.query(
+        "UPDATE products SET name = ?, price = ?, weight = ?, supplier = ?, discount = ? WHERE id = ?",
+        [name, price, weight, supplier, discount, productId]
+      );
+            res.status(200).send(updateProduct);
+    } catch (error) {
+      res.status(500).json({ message: 'Problem with updating the product' });
+    }
+  });
+  
+
+
+router.delete('/products/:id', async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const deletedProduct = await db.query("DELETE FROM products where id = ?",[productId]);
+      res.status(200).send(deletedProduct);
+    } catch (error) {
+      res.status(500).json({ message: 'Problem with deleting the product' });
+    }
+  });
+  
 
 
 export default router;
